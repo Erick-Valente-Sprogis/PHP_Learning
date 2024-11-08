70b3acff2c28ec39bb1a3354f506c2757ca0bf84
Erick Valente Sprogis <103703957+Erick-Valente-Sprogis@users.noreply.github.com>
2024-11-08 10:21:54 -0300

    Update index.php

--- index.php
+++ index.php
@@ -1,2 +1,17 @@
-<?php 
-    
+<?php
+    header("Access-Control-Allow-Origin: *");
+    header("Access-Control-Allow-Methods: GET, POST, OPTIONS");
+    header("Access-Control-Allow-Headers: Content-Type");
+
+        $peso = $_REQUEST['peso'];
+        $altura = $_REQUEST['altura'];
+        $imc = $peso / ($altura * $altura);
+
+        $resposta = [
+            "peso" => $peso,
+            "altura" => $altura,
+            "imc" => $imc,
+        ];
+
+        print(json_encode($resposta));
+    ?>
